c21a15ffb0ebd0b0d30de90b65cbb893f42476be
kaiocolombari <moscagorda2345@gmail.com>
2025-09-25 11:36:47 -0300

    mudança

--- src/app/screen/recarga.tsx
+++ src/app/screen/recarga.tsx
@@ -19,7 +19,11 @@ export default function recargaScreen() {
             </View>
 
             <ScrollView>
-                
+                <View>
+                    <Text>
+                        Recarga
+                    </Text>
+                </View>
             </ScrollView>
         </View>
     )
